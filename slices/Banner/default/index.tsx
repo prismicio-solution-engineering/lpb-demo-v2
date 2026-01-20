c9ef2faac12af87049d8c35b4d7fa6befc06cee4
guillaumeducuing <guillaumeducuing@gmail.com>
2026-01-20 09:45:54 +0100

    feat: banner

--- slices/Banner/default/index.tsx
+++ slices/Banner/default/index.tsx
@@ -1,8 +1,10 @@
-import { FC } from "react";
+import { FC, CSSProperties } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import Container from "@/components/Container";
+import { getFontTextStyles } from "@/utils/getFontStyles";
+import { EcommerceDocumentData } from "@/prismicio-types";
 
 /**
  * Props for `Banner`.
@@ -12,19 +14,53 @@ export type BannerProps = SliceComponentProps<Content.BannerSlice>;
 /**
  * Component for "Banner" Slices.
  */
-const Banner: FC<BannerProps> = ({ slice }) => {
+const Banner: FC<BannerProps> = ({ slice, context }) => {
+  const { pageData } = context as { pageData: EcommerceDocumentData };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="group relative my-20 py-20 min-h-125 overflow-hidden"
+      style={
+        {
+          ...getFontTextStyles(pageData),
+          "--primary-color": pageData.primary_color as string,
+          "--secondary-color": pageData.secondary_color as string
+        } as CSSProperties
+      }
     >
       <Container>
-        <PrismicNextImage field={slice.primary.img} />
-        <div className="bg-white">
-          <PrismicRichText field={slice.primary.title} />
-          <PrismicRichText field={slice.primary.txt} />
-          <PrismicNextLink field={slice.primary.lnk} />
-        </div>
+        <PrismicNextLink field={slice.primary.lnk}>
+          <PrismicNextImage
+            field={slice.primary.img}
+            fill
+            className="object-cover"
+          />
+          <div className="top-1/2 right-30 absolute flex flex-col gap-2 bg-white/80 p-12 max-w-70 -translate-y-1/2">
+            <PrismicRichText
+              field={slice.primary.title}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="text-(--primary-color) text-5xl leading-12">
+                    {children}
+                  </h2>
+                )
+              }}
+            />
+            <PrismicRichText
+              field={slice.primary.txt}
+              components={{
+                paragraph: ({ children }) => (
+                  <p className="text-(--primary-color) text-lg">{children}</p>
+                )
+              }}
+            />
+            <span className="text-(--primary-color) group-hover:text-(--secondary-color) underline underline-offset-3">
+              {slice.primary.lnk.text}
+            </span>
+          </div>
+        </PrismicNextLink>
       </Container>
     </section>
   );
--- slices/Banner/variation1/index.tsx
+++ slices/Banner/variation1/index.tsx
@@ -1,6 +1,10 @@
-import { FC } from "react";
+import { FC, CSSProperties } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import Container from "@/components/Container";
+import { getFontTextStyles } from "@/utils/getFontStyles";
+import { EcommerceDocumentData } from "@/prismicio-types";
 
 /**
  * Props for `Banner`.
@@ -10,19 +14,53 @@ export type BannerProps = SliceComponentProps<Content.BannerSlice>;
 /**
  * Component for "Banner" Slices.
  */
-const Banner: FC<BannerProps> = ({ slice }) => {
+const Banner: FC<BannerProps> = ({ slice, context }) => {
+  const { pageData } = context as { pageData: EcommerceDocumentData };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="group relative my-20 py-20 min-h-125"
+      style={
+        {
+          ...getFontTextStyles(pageData),
+          "--primary-color": pageData.primary_color as string,
+          "--secondary-color": pageData.secondary_color as string
+        } as CSSProperties
+      }
     >
-      Placeholder component for banner (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <Container>
+        <PrismicNextLink field={slice.primary.lnk}>
+          <PrismicNextImage
+            field={slice.primary.img}
+            className="object-cover"
+          />
+          <div className="top-1/2 right-33 absolute flex flex-col gap-2 bg-neutral-50/80 p-12 max-w-70 -translate-y-1/2">
+            <PrismicRichText
+              field={slice.primary.title}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="text-(--primary-color) text-5xl leading-12">
+                    {children}
+                  </h2>
+                )
+              }}
+            />
+            <PrismicRichText
+              field={slice.primary.txt}
+              components={{
+                paragraph: ({ children }) => (
+                  <p className="text-(--primary-color) text-lg">{children}</p>
+                )
+              }}
+            />
+            <span className="text-(--primary-color) group-hover:text-(--secondary-color) underline underline-offset-3">
+              {slice.primary.lnk.text}
+            </span>
+          </div>
+        </PrismicNextLink>
+      </Container>
     </section>
   );
 };

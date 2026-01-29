548cff268f4388da9fc51f375b8a415467e4c3be
raphael-m-prismic <raphael.mendes@prismic.io>
2026-01-29 10:58:21 +0100

    chore: contact style refinement

--- app/[lang]/recommandations/[uid]/_components/Contact.tsx
+++ app/[lang]/recommandations/[uid]/_components/Contact.tsx
@@ -22,7 +22,7 @@ export default function Contact({ data }: { data: RecapDocumentData }) {
             >
                 <div className='relative w-full px-12 rounded-2xl overflow-hidden bg-[#F5E6FF] text-gray-15 py-32 xl:px-24 shadow-sm'>
                     <div className='relative z-10 flex flex-col gap-10'>
-                        <div className='w-full sm:max-w-[60%] md:max-w-[50%] xl:max-w-[512px] flex flex-col items-start gap-4'>
+                        <div className='w-full max-w-[90%] sm:max-w-[60%] md:max-w-[50%] xl:max-w-[412px] flex flex-col items-start gap-4'>
                             {isFilled.richText(data.contact_title) ? (
                                 <PrismicRichText
                                     field={data.contact_title}
@@ -33,7 +33,7 @@ export default function Contact({ data }: { data: RecapDocumentData }) {
                                     }}
                                 />
                             ) : (
-                                <h2 id="hit-your-website-goals" className="text-4xl font-bold">Contact us</h2>
+                                <h2 id="hit-your-website-goals" className="text-4xl font-bold">Grow your website one slice at a time</h2>
                             )}
 
                             {isFilled.richText(data.contact_text) ? (
@@ -51,28 +51,28 @@ export default function Contact({ data }: { data: RecapDocumentData }) {
                         </div>
 
                         {contact && (
-                            <div className="w-full sm:w-fit p-4 flex flex-col md:flex-row justify-center items-center sm:items-start md:items-center gap-4 md:gap-8 bg-[#efd7ff] rounded-2xl">
-                                <div className='flex flex-col sm:flex-row justify-center sm:justify-start items-center gap-4'>
+                            <div className="w-full sm:w-fit flex flex-col justify-center tems-start gap-4 md:gap-4 ">
+                                <div className='flex flex-row justify-start items-start gap-4'>
                                     <div className="relative">
                                         <PrismicNextImage
                                             field={contact.image}
-                                            width={56}
-                                            height={56}
+                                            width={40}
+                                            height={40}
                                             className="object-cover aspect-square rounded-full shadow-md"
                                         />
                                      </div>
 
-                                    <div className="flex flex-col justify-center items-center sm:items-start">
-                                        <p className="font-semibold">{contact.name}</p>
-                                        <p className='text-sm'>{contact.position}</p>
+                                    <div className="flex flex-col justify-center items-start">
+                                        <p className="text-sm font-semibold">{contact.name}</p>
+                                        <p className='text-xs'>{contact.position}</p>
                                     </div>
                                 </div>
 
-                                <div className='flex flex-col sm:flex-row items-center sm:items-center gap-2'>
+                                <div className='flex flex-row flex-wrap items-start gap-2'>
                                     {isFilled.keyText(contact.email) && (
                                         <a
                                             href={`mailto:${contact.email}`}
-                                            className="text-[#FFFFFF] bg-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515] w-fit"
+                                            className="text-center text-[#FFFFFF] bg-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515]"
                                         >
                                             Email me
                                         </a>
@@ -81,7 +81,7 @@ export default function Contact({ data }: { data: RecapDocumentData }) {
                                     {isFilled.link(contact.calendar) && (
                                         <PrismicNextLink
                                             field={contact.calendar}
-                                            className="text-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515] w-fit"
+                                            className="text-center text-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515]"
                                         >
                                             { contact.calendar.text ? contact.calendar.text : "Book a call" }
                                         </PrismicNextLink>
